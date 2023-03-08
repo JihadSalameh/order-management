f52ccc63a23ac579474a944416765acc18d059b3
JihadSalameh <IMOgh3003>
2023-03-08 23:36:37 +0200

    small change

--- src/main/java/com/example/project2/Project2Application.java
+++ src/main/java/com/example/project2/Project2Application.java
@@ -2,8 +2,12 @@ package com.example.project2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+//@EnableSwagger2
 public class Project2Application {
 
 	public static void main(String[] args) {
